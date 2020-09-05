65c191cf86a80d4ab80693916de681ca34c84ed6
Alex Harsanyi <AlexHarsanyi@gmail.com>
2020-09-05 15:06:09 +0800

    Fix dependencies and add badge

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Geoid
 
+[![Build Status](https://dev.azure.com/alexharsanyi0641/racket-packages/_apis/build/status/alex-hhh.geoid?branchName=master)](https://dev.azure.com/alexharsanyi0641/racket-packages/_build/latest?definitionId=10&branchName=master)
--- info.rkt
+++ info.rkt
@@ -1,9 +1,8 @@
 #lang info
 (define collection "geoid")
-(define deps '("base"))
-(define build-deps '())
+(define deps '("base" "math-lib" "rackunit-lib" "typed-racket-lib"))
+(define build-deps '("racket-doc" "scribble-lib" "al2-test-runner"))
 (define scribblings '(("scribblings/geoid.scrbl" ())))
 (define pkg-desc "work efficiently with geographic data")
 (define version "0.0")
 (define pkg-authors '(AlexHarsanyi@gmail.com))
-
